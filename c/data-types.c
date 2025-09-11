7a97049fbc94bd18e1af2ad7b2a18483f0fa5332
lsqc <lsqc@nya.vodka>
2025-09-12 00:54:29 +0200

    add example for data types

---
+++ c/data-types.c
@@ -0,0 +1,18 @@
+int main(int argc, char** argv)
+{
+
+    int i = 1;
+
+    char c = 'c';
+
+    float f = 1.1;
+
+    double d = 2.2;
+
+    double* p = &d;
+//    printf("%p\n", p);
+
+    char s[] = "string";
+
+    return 0;
+}
